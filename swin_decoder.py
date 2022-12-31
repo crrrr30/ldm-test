0b4a6f325dde22a2268bc429a4a3fafa848c163e
crrrr30 <jonathancui03@gmail.com>
2022-12-31 07:11:32 -0800

    Add files via upload

--- swin_decoder.py
+++ swin_decoder.py
@@ -1,58 +1,20 @@
 from functools import partial
 
-import torch
+# import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from torchvision.ops.misc import Permute
 from torchvision.models.swin_transformer import SwinTransformerBlock
 
-from einops.layers.torch import Rearrange
+# from einops.layers.torch import Rearrange
 
-
-# Channel last!!
-def _patch_expanding_rearrangement(x: torch.Tensor):
-    H, W, C = x.shape[-3:]
-    assert C % 4 == 0
-    c = C // 4
-    x0 = x[..., :c]; x1 = x[..., c : 2 * c]; x2 = x[..., 2 * c : 3 * c]; x3 = x[..., 3 * c : 4 * c];
-    x = torch.empty(*x.shape[:-3], 2 * H, 2 * W, c)
-    x[..., 0::2, 0::2, :] = x0
-    x[..., 1::2, 0::2, :] = x1
-    x[..., 0::2, 1::2, :] = x2
-    x[..., 1::2, 1::2, :] = x3
-    return x
-
-
-class PatchExpanding(nn.Module):
-    """Patch Expanding Layer.
-    Args:
-        dim (int): Number of input channels.
-        norm_layer (nn.Module): Normalization layer. Default: nn.LayerNorm.
-    """
-
-    def __init__(self, dim, norm_layer=nn.LayerNorm):
-        super().__init__()
-        self.dim = dim
-        self.reduction = nn.Linear(dim, 2 * dim, bias=False)
-        self.norm = norm_layer(2 * dim)
-
-    def forward(self, x):
-        """
-        Args:
-            x (Tensor): input tensor with expected layout of [..., H, W, C]
-        Returns:
-            Tensor with layout of [..., 2*H, 2*W, C/2]
-        """
-        x = self.reduction(x)  # ... H W 2*C
-        x = self.norm(x)
-        x = _patch_expanding_rearrangement(x)       # ... H W 2*C -> ... 2*H 2*W C/2
-        return x
 
 
 class SwinTransformerBackbone(nn.Module):
     def __init__(
         self,
+        input_dim,
         embed_dim,
         depths,
         num_heads,
@@ -63,8 +25,7 @@ class SwinTransformerBackbone(nn.Module):
         attention_dropout = 0.0,
         stochastic_depth_prob = 0.1,
         norm_layer = None,
-        block = None,
-        upsample_layer = PatchExpanding,
+        block = None
     ):
         super().__init__()
 
@@ -73,9 +34,10 @@ class SwinTransformerBackbone(nn.Module):
         if norm_layer is None:
             norm_layer = partial(nn.LayerNorm, eps=1e-5)
 
-        self.patchify = nn.Sequential(
+        self.init_conv = nn.Sequential(
+            nn.Conv2d(input_dim, embed_dim, 1, 1, 0),
             Permute([0, 2, 3, 1]),
-            norm_layer(embed_dim),
+            norm_layer(embed_dim)
         )
 
         layers = []
@@ -85,7 +47,7 @@ class SwinTransformerBackbone(nn.Module):
         # build SwinTransformer blocks
         for i_stage in range(len(depths)):
             stage = []
-            dim = embed_dim // 2 ** (i_stage + 1)
+            dim = embed_dim
 
             for i_layer in range(depths[i_stage]):
                 # adjust stochastic depth probability based on the depth of the stage block
@@ -104,7 +66,8 @@ class SwinTransformerBackbone(nn.Module):
                     )
                 )
                 stage_block_id += 1
-            layers.append(nn.Sequential(upsample_layer(dim * 2, norm_layer), *stage))
+            stage = nn.Sequential(*stage)
+            layers.append(stage)
         self.features = nn.ModuleList(layers)
 
         self.norm = norm_layer(dim)
@@ -116,30 +79,14 @@ class SwinTransformerBackbone(nn.Module):
                 if m.bias is not None:
                     nn.init.zeros_(m.bias)
 
-        self.depatchify = nn.Sequential(
-            Rearrange("b (c p1 p2) h w -> b c (h p1) (w p2)", p1=patch_size, p2=patch_size),
-            nn.Conv2d(dim // patch_size ** 2, 3, 1, 1, 0)
-        )
+        self.final_conv = nn.Conv2d(dim, input_dim, 1, 1, 0)
 
     def forward(self, x):
-        x = self.patchify(x)
+        x = self.init_conv(x)
         for layer in self.features:
             x = layer(x)
         x = self.norm(x)
         x = self.permute(x)
-        x = self.depatchify(x)
+        x = self.final_conv(x)
         return x
 
-
-bb = SwinTransformerBackbone(
-    embed_dim=512,
-    depths=[4, 4, 8, 4],
-    num_heads=[16, 16, 16, 16],
-    window_size=[7, 7],
-    stochastic_depth_prob=0.2,
-)
-
-x = torch.randn(2, 512, 8, 8)
-y = bb(x)
-
-print(f"#params: {sum([w.numel() for w in bb.parameters()]):,}")
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from tqdm.notebook import tqdm
+import multiprocessing as mp
 
 import torch
 from torch.optim import AdamW
@@ -12,7 +12,64 @@ from diffusers.models import AutoencoderKL
 from swin_decoder import SwinTransformerBackbone
 from utils import *
 from dataset import get_dataloader
-from scheduler import ScheduledOptim
+
+
+class LitLatentDiffusion(pl.LightningModule):
+        def __init__(self, model_config):
+            super().__init__()
+            self.hyperparams = get_hyperparams(model_config["timesteps"], device=self.device)
+            self.num_epochs = model_config["num_epochs"]
+            self.latent_dim = model_config["latent_dim"]
+            self.latent_size = model_config["latent_size"]
+            self.max_timesteps = model_config["timesteps"]
+            self.model = SwinTransformerBackbone(
+                input_dim=4,
+                embed_dim=256,
+                depths=[4, 4, 8, 4],
+                num_heads=[16, 16, 16, 16],
+                window_size=[3, 3],
+                stochastic_depth_prob=0.2,
+            )
+            self.vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse").to(device)
+            self.vae.requires_grad = False
+            self.save_hyperparameters(ignore=["vae"])
+        def forward(self, x):
+            return self.model(x)
+        def configure_optimizers(self):
+            optimizer = AdamW(self.model.parameters(), lr=1e-6, weight_decay=1e-2)
+            scheduler = LinearLR(
+                optimizer,
+                start_factor=0.0015,
+                end_factor=0.0195,
+                total_iters=self.trainer.estimated_stepping_batches * self.num_epochs
+            )
+            return [optimizer], [scheduler]
+        def training_step(self, data, idx):
+            sch = self.lr_schedulers()
+            sch.zero_grad()
+            sch.step()
+            b = data.shape[0]
+            if data.shape[-1] != self.image_size:
+                data = F.interpolate(data, self.image_size, mode="bilinear", align_corners=True)
+            t = torch.randint(0, max_timesteps, size=(b, ), device=self.device)
+            with torch.no_grad():
+                latents = self.vae.encoder(data).mode()
+            loss = p_losses(self.hyperparams, self.model, latents, t)
+            self.log("train_loss", loss, on_epoch=True)
+            return loss
+        def on_save_checkpoint(self, checkpoint):
+            print(checkpoint)
+            # sample_and_save(self.hyperparams, self.model, self.vae, step, self.latent_size, num=8)
+        def validation_step(self, data, idx):
+            b = data.shape[0]
+            with torch.no_grad():
+                latents = self.vae.encode(data).mode()
+            losses = []
+            for t in range(self.max_timesteps):
+                t = torch.Tensor([t] * b, device=self.device)
+                loss = p_losses(self.hyperparams, self.model, latents, t)
+                losses.append(loss)
+            self.log("val_l1_loss", torch.mean(losses))
 
 
 device = "cuda"
@@ -34,62 +91,6 @@ max_timesteps = model_config["timesteps"]
 
 dataloader = get_dataloader(batch_size=batch_size)
 
-class LitLatentDiffusion(pl.LightningModule):
-    def __init__(self, model_config):
-        super().__init__()
-        self.hyperparams = get_hyperparams(model_config["timesteps"], device=self.device)
-        self.num_epochs = model_config["num_epochs"]
-        self.latent_dim = model_config["latent_dim"]
-        self.latent_size = model_config["latent_size"]
-        self.max_timesteps = model_config["max_timesteps"]
-        self.model = SwinTransformerBackbone()
-        self.vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse").to(device)
-        self.vae.freeze()
-        self.save_hyperparameters(ignore=["vae"])
-    def forward(self, x):
-        return self.model(x)
-    def configure_optimizers(self):
-        optimizer = AdamW(self.model.parameters(), lr=1e-6, weight_decay=1e-2)
-        scheduler = LinearLR(
-            optimizer,
-            start_factor=0.0015,
-            end_factor=0.0195,
-            total_iters=self.trainer.estimated_stepping_batches * self.num_epochs
-        )
-        return {
-                "optimizer": optimizer,
-                "lr_scheduler": {
-                    "scheduler": scheduler,
-                    # "monitor": "metric_to_track",
-                },
-            }
-    def training_step(self, data, idx):
-        sch = self.lr_schedulers()
-        sch.zero_grad()
-        sch.step()
-        b = data.shape[0]
-        if data.shape[-1] != self.image_size:
-            data = F.interpolate(data, self.image_size, mode="bilinear", align_corners=True)
-        t = torch.randint(0, max_timesteps, size=(b, ), device=self.device)
-        with torch.no_grad():
-            latents = self.vae.encoder(data).mode()
-        loss = p_losses(self.hyperparams, self.model, latents, t)
-        self.log("train_loss", loss, on_epoch=True)
-        return loss
-    def on_save_checkpoint(self, checkpoint):
-        print(checkpoint)
-        # sample_and_save(self.hyperparams, self.model, self.vae, step, self.latent_size, num=8)
-    def validation_step(self, data, idx):
-        b = data.shape[0]
-        with torch.no_grad():
-            latents = self.vae.encode(data).mode()
-        losses = []
-        for t in range(self.max_timesteps):
-            t = torch.Tensor([t] * b, device=self.device)
-            loss = p_losses(self.hyperparams, self.model, latents, t)
-            losses.append(loss)
-        self.log("val_l1_loss", torch.mean(losses))
-
 
 tb_logger = pl.loggers.TensorBoardLogger(
     "lightning_logs/",
@@ -103,7 +104,7 @@ trainer = pl.Trainer(
     devices=torch.cuda.device_count(),
     accelerator="gpu",
     callbacks=[],
-    logger=tb_logger
+    logger=tb_logger,
 )
 
 trainer.fit(model, dataloader)
